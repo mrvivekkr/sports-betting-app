ea75f26ed1fa8246d4981fef59fa7cfeb0ff0765
Vivek Kumar <vivekkrmgr@gmail.com>
2025-08-02 13:48:01 +0530

    refactor: Clean up unused import

--- src/main/java/com/sporty/betting/controller/EventOutcomeController.java
+++ src/main/java/com/sporty/betting/controller/EventOutcomeController.java
@@ -3,7 +3,6 @@ package com.sporty.betting.controller;
 import com.sporty.betting.model.EventOutcome;
 import com.sporty.betting.service.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
